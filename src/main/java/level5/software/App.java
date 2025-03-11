87ad4a06995e27a02f80a53a93fc9fa1a461fdaf
jan shah <shahj@ucenmanchester.ac.uk>
2025-03-11 16:28:14 +0000

    update stage

--- src/main/java/level5/software/App.java
+++ src/main/java/level5/software/App.java
@@ -4,22 +4,58 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private GameBoard board;
+    private List<Button> cards = new ArrayList<>();
+
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        double pairs = 24;
+
+        GridPane grid = new GridPane();
+        grid.setHgap(2);
+        grid.setVgap(2);
+        int rows = (int) Math.sqrt(pairs);
+        int cols = (int) Math.ceil(pairs / rows);
+        int cardWidth = 80;
+        int cardHeight = 100;
+        board = new GameBoard(pairs);
+        List<Button> cards = new ArrayList<>();
+
+        for (Card card : board.getCards()) {
+            Button btn = new Button("?");
+            btn.setMinWidth(cardWidth - 2);
+            btn.setMinHeight(cardHeight - 2);
+            cards.add(btn);
+
+            int index = card.getIndex();
+
+            btn.setOnAction(e -> checkCard(card, btn));
+
+            grid.add(btn, index % cols, index / cols);
+        }
+
+        scene = new Scene(grid, cols * cardWidth, rows * (cardHeight * 2));
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void checkCard(Card card, Button btn) {
+        System.out.println(card.getValue());
     }
 
     static void setRoot(String fxml) throws IOException {
