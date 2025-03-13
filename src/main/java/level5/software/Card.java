88c16b8cccbf504c518bd6981a4776d6773e9814
toby sykes <tobysykes05@gmail.com>
2025-03-13 15:12:32 +0000

    added flipper to card

--- src/main/java/level5/software/Card.java
+++ src/main/java/level5/software/Card.java
@@ -3,6 +3,7 @@ package level5.software;
 public class Card {
     private final int index;
     private final String value;
+    private boolean showing = false;
     
     public Card(int index, String value) {
         this.index = index;
@@ -15,5 +16,12 @@ public class Card {
 
     public String getValue() {
         return value;
+    }
+
+    public void flip(){
+        showing = !showing;
+    }
+    public boolean isFlipped(){
+        return showing;
     }
 }
